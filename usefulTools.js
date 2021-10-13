7e70cd1bb2a72d4708dc9fb580612098301fd53a
fogre <finnogre@gmail.com>
2021-10-13 15:46:31 +0300

    workshop 2

---
+++ usefulTools.js
@@ -0,0 +1,18 @@
+// Find if childs parents has overflow set
+const selector = '.the-fixed-child';
+function findCulprits(elem) {
+  if (!elem) {
+    throw new Error(
+      'Could not find element with that selector'
+    );
+  }
+  let parent = elem.parentElement;
+  while (parent) {
+    const hasOverflow = getComputedStyle(parent).overflow;
+    if (hasOverflow !== 'visible') {
+      console.log(hasOverflow, parent);
+    }
+    parent = parent.parentElement;
+  }
+}
+findCulprits(document.querySelector(selector));
