701f34be34bc516d93c5e4964e1fbdd03b0877b7
fogre <finnogre@gmail.com>
2021-10-24 12:17:48 +0300

    fix missing ,

--- 06. unsprinkle/src/components/Picture/Picture.js
+++ 06. unsprinkle/src/components/Picture/Picture.js
@@ -3,15 +3,17 @@ import React from 'react';
 const Picture = ({ src, children }) => {
   const avifSrcset = `
     ${src.replace('.jpg', '.avif')},
-    ${src.replace('.jpg', '@2x.avif 2x')}
+    ${src.replace('.jpg', '@2x.avif 2x')},
     ${src.replace('.jpg', '@3x.avif 3x')}
   `;
 
   const jpgSrcset = `
     ${src},
-    ${src.replace('.jpg', '@2x.jpg 2x')}
+    ${src.replace('.jpg', '@2x.jpg 2x')},
     ${src.replace('.jpg', '@3x.jpg 3x')}
   `;
+
+  console.log(jpgSrcset);
 
   return (
     <picture>
