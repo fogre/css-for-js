439c32b9b870f784ac09643df9a72dee2bcb24ca
fogre <finnogre@gmail.com>
2021-10-24 11:58:36 +0300

    workshop 6

---
+++ 06. unsprinkle/src/components/PhotoGridItem/PhotoGridItem.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+import Picture from '../Picture'
+
+const PhotoGridItem = ({ id, src, alt, tags }) => {
+  return (
+    <article>
+      <Anchor href={`/photos/${id}`}>
+        <Picture src={src}>
+          <Image src={src} alt={alt}/>
+        </Picture>
+      </Anchor>
+      <Tags>
+        {tags.map((tag) => (
+          <Tag key={tag}>{tag}</Tag>
+        ))}
+      </Tags>
+    </article>
+  );
+};
+
+const Anchor = styled.a`
+  text-decoration: none;
+  color: inherit;
+  outline-offset: 4px;
+`;
+
+const Image = styled.img`
+  display: block;
+  width: 100%;
+  height: 300px;
+  border-radius: 2px;
+  margin-bottom: 8px;
+  object-fit: cover;
+`;
+
+const Tags = styled.ul`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  height: 100%;
+  line-height: 1.5;
+`;
+
+const Tag = styled.li`
+  display: inline;
+  padding: 4px 8px;
+  background: var(--color-gray-300);
+  font-size: 0.875rem;
+  font-weight: 475;
+  color: var(--color-gray-800);
+
+  &:not(:first-of-type) {
+    margin-left: 8px;
+  }
+`;
+
+export default PhotoGridItem;
